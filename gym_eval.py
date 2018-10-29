d81b7195e49095f873a81c6e55b87d9a67648a5b
sajan kumar <ksajan12@gmail.com>
2018-10-29 17:47:15 +0530

    bug dissolved

--- gym_eval.py
+++ gym_eval.py
@@ -5,7 +5,7 @@ import argparse
 import torch
 from environment import create_env
 from utils import setup_logger
-from model import A3C_CONV, A3C_MLP
+from model import A3C_MLP
 from player_util import Agent
 from torch.autograd import Variable
 import gym
